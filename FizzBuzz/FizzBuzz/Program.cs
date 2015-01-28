efec7386e5c5bbb251811305c78e758aa2d793de
Eric Fowler <ericfowler303@gmail.com>
2015-01-28 12:17:15 -0700

    Initial commit
    Project contains a blank FizzBuzz method with a test class attached.

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 1; i < 20; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }*/
+        }
+        public static string FizzBuzz(int number)
+        {
+            return "";
+        }
+    }
+}
